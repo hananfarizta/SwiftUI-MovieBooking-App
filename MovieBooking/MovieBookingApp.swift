1bf3f5b11dfa32cc22d1472e0ea45255c3c7dbff
hananfarizta <hananfariztaa@gmail.com>
2024-06-24 10:47:09 +0700

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Hanan Farizta on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
